0424b7df492a29a8c3829908e2f9fbff8be44c7d
Mykhailo Zheliaskov <mykhailo.zheliaskov@decerto.pl>
2016-10-25 01:15:03 +0200

    Connect to mesql and show users form db

---
+++ models/user.js
@@ -0,0 +1,54 @@
+var db = require('../lib/mysql');
+
+// function User(data) {
+//     this.id = data.id;
+//     this.username = data.username;
+//     this.email = data.email;
+//     this.firstname = data.firstname;
+//     this.secondname = data.secondname;
+//
+//     //TODO доработать этот момент. Есть подозрение что пароль не долже храниться в объекте
+//     this.hashed_password = data.hashed_password;
+//     this.salt = data.salt;
+// }
+
+module.exports = {
+
+    findById: function(userId, callback) {
+        db.getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                throw err;
+            }
+            var sql = 'SELECT * FROM user WHERE id = ' + connection.escape(parseInt(userId));
+            connection.query(sql, function(err, user){
+                connection.release();
+                if(!err) {
+                    callback(null, {user: user});
+                }
+            });
+            connection.on('error', function(err) {
+                throw err;
+            });
+        });
+    },
+
+    find: function(callback) {
+        db.getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                throw err;
+            }
+            var sql = 'SELECT * FROM user';
+            connection.query(sql, function(err, users){
+                connection.release();
+                if(!err) {
+                    callback(null, {users: users});
+                }
+            });
+            connection.on('error', function(err) {
+                throw err;
+            });
+        });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function (req, res, next) {
+    User.find(function (err, users) {
+        if (err) return next(err);
+        res.json(users);
+    });
+});
+
+router.get('/user/:id', function (req, res, next) {
+    User.findById(req.params.id, function(err, user) {
+        if (err) return next(err);
+        res.json(user);
+    });
 });
 
 module.exports = router;
