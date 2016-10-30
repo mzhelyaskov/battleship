55b8180d41aad2c3bc4c8c1029ecb9146f3d50bd
Mykhailo Zheliaskov <mykhailo.zheliaskov@decerto.pl>
2016-10-30 15:55:25 +0100

    Created user model and methods

--- models/user.js
+++ models/user.js
@@ -1,19 +1,44 @@
 var db = require('../lib/mysql');
+const crypto = require('crypto');
 
-// function User(data) {
-//     this.id = data.id;
-//     this.username = data.username;
-//     this.email = data.email;
-//     this.firstname = data.firstname;
-//     this.secondname = data.secondname;
-//
-//     //TODO доработать этот момент. Есть подозрение что пароль не долже храниться в объекте
-//     this.hashed_password = data.hashed_password;
-//     this.salt = data.salt;
-// }
+function User(data) {
+
+    this.id = data.id;
+    this.username = data.username;
+    this.email = data.email;
+    this.firstname = data.firstname;
+    this.secondname = data.secondname;
+    this.hashedPassword = data.hashedPassword;
+
+    var _salt = data.salt;
+    var _plainPassword;
+
+    this.checkPassword = function (password) {
+        return encryptPassword(password) === this.hashedPassword;
+    };
+
+    this.getPassword = function() {
+        return _plainPassword;
+    };
+
+    this.setPassword = function(password) {
+        _plainPassword = password;
+        _salt = Math.random() + '';
+        this.hashedPassword = encryptPassword(password);
+    };
+
+    function encryptPassword(password) {
+        return crypto.createHmac('sha256', _salt)
+            .update('There can be only one!')
+            .digest('hex');
+    }
+
+    this.save = function() {
+        // TODO ... реализовать функцию сохранения в базу
+    };
+}
 
 module.exports = {
-
     findById: function(userId, callback) {
         db.getConnection(function(err, connection) {
             if (err) {
@@ -25,7 +50,8 @@ module.exports = {
             connection.query(sql, function(err, users){
                 connection.release();
                 if(!err) {
-                    callback(null, users[0]);
+                    var user = users[0] && new User(users[0]);
+                    callback(null, user);
                 }
             });
             connection.on('error', function(err) {
@@ -45,7 +71,28 @@ module.exports = {
             connection.query(sql, function(err, users){
                 connection.release();
                 if(!err) {
-                    callback(null, users);
+                    callback(null, users.map(function(userData) {new User(userData)}));
+                }
+            });
+            connection.on('error', function(err) {
+                callback(err);
+            });
+        });
+    },
+
+    findByName: function(username, callback) {
+        db.getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                callback(err);
+                return;
+            }
+            var sql = 'SELECT * FROM user WHERE username = ' + connection.escape(username);
+            connection.query(sql, function(err, users){
+                connection.release();
+                if(!err) {
+                    var user = users[0] && new User(users[0]);
+                    callback(null, user);
                 }
             });
             connection.on('error', function(err) {
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function(req, res, next) {
     res.render('login');
@@ -8,7 +9,22 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
-    
+    User.findByName(username, function(err, user) {
+        if (err) return next(err);
+        if (user) {
+            if (user.checkPassword(password)) {
+                // ... 200 ОК
+            } else {
+                // ... 403 Forbiden
+            }
+        } else {
+            var user = new User({username: username, password: password});
+            user.save(function(err) {
+                if (err) return next(err);
+                // ... 200
+            });
+        }
+    });
     res.render('login');
 });
 
