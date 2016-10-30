46ae7efa84bf282cfcee8b07d188415218f4b19b
Mykhailo Zheliaskov <mykhailo.zheliaskov@decerto.pl>
2016-10-30 11:54:19 +0100

    Created lgin page and login routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var HttpError = require('./error');
 // routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // routing
 app.use('/', routes);
 app.use('/', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
